8aa5e5dd456ddf305a49787704bdb13d5d136e9f
Michał Zieliński <michal@zielinscy.org.pl>
2012-05-26 21:25:16 +0200

    Added license notice.

--- gitjoin/authorized_keys.py
+++ gitjoin/authorized_keys.py
@@ -1,3 +1,10 @@
+# Gitjoin Git Management
+# Copyright (C) 2012 Michal Zielinski
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 import tools
 import os
 import StringIO
--- gitjoin/git_auth.py
+++ gitjoin/git_auth.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+# Gitjoin Git Management
+# Copyright (C) 2012 Michal Zielinski
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 import sys
 
 sys.stdout = sys.stderr # but do not redirect native stdout
