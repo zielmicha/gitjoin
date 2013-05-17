b151d8dddf41d566db1032837219fd055a8d141f
Michał Zieliński <michal@zielinscy.org.pl>
2013-05-17 23:26:16 +0200

    more modular sshd.py

--- gitjoin/sshd.py
+++ gitjoin/sshd.py
@@ -24,8 +24,8 @@ import pwd
 
 from . import tools
 
-username_map = {'git': 1002}
-gid = None
+class settings: pass
+settings = settings()
 
 def nothing():
     pass
@@ -44,7 +44,7 @@ class KeySession:
 
     def execCommand(self, proto, cmd):
         username, key_id = self.avatar.avatarId
-        uid = username_map[username]
+        uid = settings.username_get(username)
         environ = {}
         home = pwd.getpwuid(uid).pw_dir
         environ['SSH_ORIGINAL_COMMAND'] = cmd
@@ -54,7 +54,7 @@ class KeySession:
         assert "'" not in key_id # generate by get_ssh_key_fingerprint, so it's safe
         self.pty = reactor.spawnProcess(proto,
                                         '/bin/sh', ['sh', '-c', "python -m gitjoin.git_auth '%s'" % key_id], environ, home,
-                                        uid, gid)
+                                        uid, settings.gid)
         self.avatar.conn.transport.transport.setTcpNoDelay(1)
 
     def closed(self):
@@ -79,12 +79,15 @@ class KeyChecker(object):
         id = tools.get_ssh_key_fingerprint(pubkey)
         return (credentials.username, id)
 
-if __name__ == '__main__':
-    with open('id_rsa') as privateBlobFile:
+def main(keys_path, username_get, gid, port=2022):
+    settings.username_get = username_get
+    settings.gid = gid
+
+    with open(keys_path + '/id_rsa') as privateBlobFile:
         privateBlob = privateBlobFile.read()
         privateKey = Key.fromString(data=privateBlob)
 
-    with open('id_rsa.pub') as publicBlobFile:
+    with open(keys_path + '/id_rsa.pub') as publicBlobFile:
         publicBlob = publicBlobFile.read()
         publicKey = Key.fromString(data=publicBlob)
 
@@ -94,5 +97,8 @@ if __name__ == '__main__':
     factory.portal = Portal(KeyRealm())
     factory.portal.registerChecker(KeyChecker())
 
-    reactor.listenTCP(2022, factory)
+    reactor.listenTCP(port, factory)
     reactor.run()
+
+if __name__ == '__main__':
+    main('.', lambda name: 1002, 100)
