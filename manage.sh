f2861d82753ea7d10703c3bf105fba3309bc7fec
Michał Zieliński <michal@zielinscy.org.pl>
2014-01-13 18:26:28 +0100

    update setup scripts

--- manage.sh
+++ manage.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
-if [ ! -e virtualenv ]; then
-    virtualenv virtualenv
-    ./build_libgit.sh
-fi
 . activate.inc
-pip install -r requirements.txt
 
 python manage.py "$@"
