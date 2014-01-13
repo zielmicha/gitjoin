79fb0314a8cc0cc6f1b0c7fc1568fb6dcbff15cc
Michał Zieliński <michal@zielinscy.org.pl>
2014-01-13 23:06:18 +0100

    uwsgi config

--- webapp/wsgi.py
+++ webapp/wsgi.py
@@ -16,7 +16,7 @@ framework.
 import os
 import sys
 
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.dirname(__file__) + '/..')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webapp.settings")
 
 # This application object is used by any WSGI server configured to use this
